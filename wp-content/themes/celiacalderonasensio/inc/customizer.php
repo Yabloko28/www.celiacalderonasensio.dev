be8101a17d10a776f73fc78545cc59240593a6fd
nadyaPeeps <nadezhda.fed.28@gmail.com>
2016-03-18 19:47:28 -0400

    setting up wordpress theme with angular

---
+++ wp-content/themes/celiacalderonasensio/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * celiacalderonasensio Theme Customizer.
+ *
+ * @package celiacalderonasensio
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function celiacalderonasensio_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'celiacalderonasensio_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function celiacalderonasensio_customize_preview_js() {
+	wp_enqueue_script( 'celiacalderonasensio_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'celiacalderonasensio_customize_preview_js' );
---
+++ wp-content/themes/celiacalderonasensio/js/angular-theme.js
@@ -0,0 +1,75 @@
+var wpApp = new angular.module('wpAngularTheme', ['ui.router', 'ngResource']);
+
+wpApp.factory('Posts', function($resource) {
+	return $resource(appInfo.api_url + 'posts/:ID', {
+		ID: '@id'
+	})
+});
+
+wpApp.factory('Pages', function($resource) {
+	return $resource(appInfo.api_url + 'pages/?slug=about')
+});
+
+wpApp.controller('FashionCtrl', ['$scope', 'Posts', function($scope, Posts) {
+	console.log('works');
+	$scope.page_title = 'Blog Listing';
+    Posts.query(function(res) {
+    	$scope.posts = res;
+    });
+}])
+
+wpApp.controller('OtherCtrl', ['$scope', 'Posts', function($scope, Posts) {
+	console.log('OtherCtrl');
+    Posts.query(function(res) {
+    	$scope.posts = res;
+    	console.log($scope.posts);
+    });
+
+}])
+
+wpApp.controller('ProjectCtrl', ['$scope', '$stateParams', 'Posts', function($scope, $stateParams, Posts) {
+	console.log($stateParams);
+	Posts.get({ID: $stateParams.id}, function(res) {
+		$scope.post = res;
+		console.log(res);
+	})
+	
+}])
+
+wpApp.controller('AboutCtrl', ['$scope', '$stateParams', 'Pages', function($scope, $stateParams, Pages) {
+	 Pages.query(function(res) {
+    	$scope.page = res[0];
+    	console.log($scope.page.content.rendered);
+    });
+}])
+
+wpApp.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
+	$stateProvider
+	       .state('fashion', {
+	       	   url: '/',
+	       	   controller: 'FashionCtrl',
+	       	   templateUrl: appInfo.template_directory + 'templates/fashion.html'
+	       })
+	       .state('other-projects', {
+	       	   url: '/other-projects',
+	       	   controller: 'OtherCtrl',
+	       	   templateUrl: appInfo.template_directory + 'templates/other.html'
+	       })
+	       .state('project', {
+	        	url: '/fashion/:id',
+	        	controller: 'ProjectCtrl',
+	        	templateUrl: appInfo.template_directory + 'templates/project.html'
+	        })
+	       .state('about', {
+	        	url: '/about',
+	        	controller: 'AboutCtrl',
+	        	templateUrl: appInfo.template_directory + 'templates/about.html'
+	        })
+})
+
+wpApp.filter('to_trusted', ['$sce', function($sce) {
+	return function(text) {
+		return $sce.trustAsHtml(text);
+	}
+}])
